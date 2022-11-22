03e3afee30687c874843542a6952f0f2aea64ab2
KChi2000 <hachiqhth20@gmail.com>
2022-11-22 08:43:29 +0700

    first commit

---
+++ lib/Tracking.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_map/plugin_api.dart';
+
+import 'package:latlong2/latlong.dart';
+
+class Tracking extends StatelessWidget {
+  Tracking({super.key});
+  List<LatLng> polylineCoordinates = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Map',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.yellow,
+      ),
+      body:
+       FlutterMap(
+        options: MapOptions(
+          center: LatLng(21.0117040, 105.8114780),
+          zoom: 5,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.map',
+          ),
+          PolygonLayer(
+            polygonCulling: false,
+            polygons: [
+              Polygon(points: [
+                LatLng(30, 40),
+                LatLng(20, 50),
+                LatLng(25, 45),
+              ], color: Colors.blue, borderStrokeWidth: 3),
+            ],
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                  point: LatLng(21.0117040, 105.8114780),
+                  width: 10,
+                  height: 10,
+                  rotate: true,
+                  builder: (context) {
+                    print('markerrrr');
+                    return Icon(
+                      Icons.location_on,
+                      color: Colors.red[700],
+                    );
+                  }),
+              Marker(
+                  point: LatLng(20, 50),
+                  width: 10,
+                  height: 10,
+                  rotate: true,
+                  builder: (context) {
+                    print('markerrrr');
+                    return Icon(
+                      Icons.location_on,
+                      color: Colors.red[700],
+                    );
+                  }),
+              Marker(
+                  point: LatLng(25, 45),
+                  width: 10,
+                  height: 10,
+                  rotate: true,
+                  builder: (context) {
+                    print('markerrrr');
+                    return Icon(
+                      Icons.location_on,
+                      color: Colors.red[700],
+                    );
+                  }),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.yellow,
+        child: Icon(Icons.message,color: Colors.black,),
+      ),
+    );
+  }
+}
